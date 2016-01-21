75d8803ef2e11f3f5df576003d48d1ecaa190415
WangRunshi <shinate@WangRunshideMacBook-Pro.local>
2016-01-21 17:31:19 +0800

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+
+comments=$1
+if [ -z $1 ];then
+    comments = "new post"
+fi
+
 gulp
 jekyll build
 git add .
-git commit -m "new post"
+git commit -m "${comments}"
 git push origin gh-pages
